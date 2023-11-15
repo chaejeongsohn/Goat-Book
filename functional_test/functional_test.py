d2c12d465ad04b14a4b9a65b697d2de23dc0fda7
ChaeJeong <pagw1604@gmail.com>
2023-11-15 10:34:09 +0900

    05. Django 모델과 POST 구현

--- functional_test/functional_test.py
+++ functional_test/functional_test.py
@@ -9,10 +9,6 @@ import tracemalloc
 tracemalloc.start()
 
 
-
-# 02. unittest 모듈로 기능 테스트 확장
-# unittest.TestCase 사용
-# https://docs.python.org/3/library/unittest.html
 class NewVisitorTest(unittest.TestCase):
     # 테스트 시작 전
     def setUp(self):
@@ -22,14 +18,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    # test_ 로 시작되는 메소드 = test 메소드 라는 의미
-    # 04. Test로 할 수 있는 것들
-    # Selenium 사용
-    # - find_element
-    # - find_elements
-    # - By.SOMETHING : HTML 속성을 사용하여 검색할 수 있도록 매개변수화함
-    # - send_keys: input 요소에 입력함
-    # - Keys : Enter와 같은 특수 키 보내기
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element(By.ID, "id_list_table")
+        rows = table.find_elements(By.TAG_NAME, "tr")
+        self.assertIn(row_text, [row_text for row in rows])
+
     def test_can_start_a_todo_list(self):
         # 홈페이지 방문
         self.browser.get("http://localhost:8000")
@@ -38,28 +31,25 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element(By.TAG_NAME, "h1").text
         self.assertIn("To-Do", header_text)
 
+
         inputbox = self.browser.find_element(By.ID, "id_new_item")
         self.assertEqual(inputbox.get_attribute("placeholder"),"Enter a to-do item")
-
+        
+        # 첫번째 입력
         inputbox.send_keys("Buy peacock feathers")
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)  # 명시적 대기(브라우저 로드 용도)
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
 
-        table = self.browser.find_element(By.ID, "id_list_table")
-        rows = table.find_elements(By.TAG_NAME, "tr")
+        # 두번째 입력
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("Use peacock feathers to make a fly")
+        inputbox.send_keys(Keys.ENTER)   
+        time.sleep(1)  # 명시적 대기(브라우저 로드 용도)
 
-        # self.assertTrue(any(row.text == "1.Buy peacock feathers" for row in rows),
-        #                 f"table에 새로운 to-do item이 없습니다. 입력받은 내용:\n{table.text}",)
-        # 같은 내용을 아래 self.assertIn 로 구현
-        self.assertIn(
-            "1: Buy peacock feathers",
-            [row.text for row in rows]
-        )
-        self.assertIn(
-            "2: Use peacock feathers to make a fly",
-            [row.text for row in rows],
-        )
-        
+        # 입력 테이블 확인
+        self.check_for_row_in_list_table("1: Buy peacock feathers")
+        self.check_for_row_in_list_table("2: Use peacock feathers to make a fly")
 
 
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,24 +1,45 @@
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from lists.models import Item
 
-# 03. Unit tests(단위 테스트)
-# Functional tests
-# - 사용자 관점에서 테스트
-# - 실제로 작동하는 애플리케이션을 구축하는 데 도움이 되며 실수로 애플리케이션이 중단되지 않도록 보장
-# - 앱 동작의 모든 작은 세부 사항을 다루는 것이 아니라 모든 것이 올바르게 연결되어 있는지 확인하기 위한 것입니다.
-# Unit tests
-# - 코드 테스트, 즉 프로그래머의 관점
-# - 깨끗하고 버그가 없는 코드를 작성하는 데 도움
-# - 모든 하위 수준 세부 사항과 특수 사례를 철저하게 확인하기 위해 존재합니다.
 
 class HomePageTest(TestCase):
-
     def test_uses_home_template(self):
         response = self.client.get("/")
         self.assertTemplateUsed(response,"home.html")  # 어떤 템플릿이 사용되는지 확인
 
     def test_can_save_a_POST_request(self):
         response = self.client.post("/", data={"item_text":"A new list item"})  # POST 실행
+
+        self.assertEqual(Item.objects.count(), 1)  # Item 하나가 데이터베이스에 저장되었는지 확인
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "A new list item")  # 입력이 잘들어갔는지 확인
+
         self.assertContains(response, "A new list item")  
         self.assertTemplateUsed(response, "home.html")  # 어떤 템플릿이 사용되는지 확인
+
+        self.assertRedirects(response, "/")
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get("/")
+        self.assertEqual(Item.objects.count(), 0)
+
+
+class ItemModelTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = "첫번째 아이템"
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "두번째 아이템"
+        second_item.save()
+
+        saved_items = Item.objects.all()  # Django 데이터베이스 쿼리
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "첫번째 아이템")
+        self.assertEqual(second_saved_item.text, "두번째 아이템")
