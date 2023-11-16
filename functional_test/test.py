63532a31b2b14b9fc1a6d11d2914c1dca3f8db1e
ChaeJeong <pagw1604@gmail.com>
2023-11-16 17:19:24 +0900

    08. 레이아웃 + 스타일링을 위한 Functional Test 작성

--- functional_test/test.py
+++ functional_test/test.py
@@ -5,7 +5,6 @@ from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-# from chromedriver_autoinstaller import install
 
 MAX_WAIT = 5
 
@@ -13,7 +12,6 @@ MAX_WAIT = 5
 class NewVisitorTest(LiveServerTestCase):
     # 테스트 시작 전
     def setUp(self):
-        # install()   # Install chromedriver if not already installed
         self.browser = webdriver.Firefox()
 
     # 테스트 완료 후
@@ -96,6 +94,25 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element(By.TAG_NAME, "body").text
         self.assertNotIn("공작 깃털 구매", page_text)
         self.assertIn("우유 구매", page_text)
+
+    def test_layout_and_styling(self):
+        # Lisa가 홈페이지 방문
+        self.browser.get(self.live_server_url)
+
+        # Lisa의 브라우저 윈도우 크기 지정
+        self.browser.set_window_size(1024, 768)
+
+        # Lisa는 input박스가 가운데 정렬됨을 발견하고, 입력함
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        inputbox.send_keys("테스트")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: 테스트")
+
+        # Lisa는 새 목록의 페이지에서도 input박스가 가운데 정렬됨을 발견함
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertAlmostEqual(  # 연산의 오차범위를 10픽셀로 지정 > 반올림 오류, 스크롤바 등으로 인한 현상 처리 도움
+            inputbox.location["x"] + inputbox.size["width"] / 2, 512, delta=10, 
+        )
 
 
 
