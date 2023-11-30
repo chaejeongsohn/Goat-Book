f216e16be98f98dbf0e778b5a217276f663470a5
ChaeJeong <pagw1604@gmail.com>
2023-11-30 17:48:13 +0900

    12. Unit Test: 두개의 파일로 분할

---
+++ lists/tests/test_models.py
@@ -0,0 +1,32 @@
+from django.test import TestCase
+from lists.models import Item, List
+
+
+
+class ListAndItemModelsTest(TestCase):
+    def test_saving_and_retrieving_items(self):
+        mylist = List()
+        mylist.save()
+
+        first_item = Item()
+        first_item.text = "첫번째 아이템"
+        first_item.list = mylist
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = "두번째 아이템"
+        second_item.list = mylist
+        second_item.save()
+
+        saved_list = List.objects.get()
+        self.assertEqual(saved_list, mylist)
+
+        saved_items = Item.objects.all()  # Django 데이터베이스 쿼리
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, "첫번째 아이템")
+        self.assertEqual(first_saved_item.list, mylist)
+        self.assertEqual(second_saved_item.text, "두번째 아이템")
+        self.assertEqual(second_saved_item.list, mylist)
--- lists/tests/test_veiws.py
+++ lists/tests/test_veiws.py
@@ -4,10 +4,6 @@ from lists.views import home_page
 from lists.models import Item, List
 
 
-# 07. 점진적으로 작업하기
-# Unit test가 통과했지만 Functional test는 통과하지 못한 경우,
-# Unit test에서 다루지 않은 문제를 가리키는 경우가 많으며
-# 우리의 경우에는 템플릿 문제인 경우가 많다는 경험 법칙입니다.
 
 ## / 루트 페이지: 새로운 목록 생성, 표시
 class HomePageTest(TestCase):
@@ -42,6 +38,12 @@ class ListViewTest(TestCase):
         response = self.client.get(f"/lists/{mylist.id}/")
         self.assertTemplateUsed(response, "list.html")
 
+    def test_passes_correct_list_to_template(self):
+        other_list =List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get(f"/lists/{correct_list.id}/")
+        self.assertEqual(response.context["list"],correct_list)
+
     # List View 페이지에 모든 항목이 있는지 확인
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
@@ -54,14 +56,6 @@ class ListViewTest(TestCase):
         self.assertContains(response, "두번째 아이템")
         self.assertNotContains(response, "다른 리스트 아이템")
 
-    def test_passes_correct_list_to_template(self):
-        other_list =List.objects.create()
-        correct_list = List.objects.create()
-        response = self.client.get(f"/lists/{correct_list.id}/")
-        self.assertEqual(response.context["list"],correct_list)
-
-
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         other_list = List.objects.create()
         correct_list = List.objects.create()
@@ -81,32 +75,3 @@ class NewItemTest(TestCase):
         response = self.client.post(f"/lists/{correct_list.id}/add_item",
                                     data={"item_text":"이미 존재하는 list에 새로운 Item 추가"})
         self.assertRedirects(response, f"/lists/{correct_list.id}/")
-
-
-class ListAndItemModelsTest(TestCase):
-    def test_saving_and_retrieving_items(self):
-        mylist = List()
-        mylist.save()
-
-        first_item = Item()
-        first_item.text = "첫번째 아이템"
-        first_item.list = mylist
-        first_item.save()
-
-        second_item = Item()
-        second_item.text = "두번째 아이템"
-        second_item.list = mylist
-        second_item.save()
-
-        saved_list = List.objects.get()
-        self.assertEqual(saved_list, mylist)
-
-        saved_items = Item.objects.all()  # Django 데이터베이스 쿼리
-        self.assertEqual(saved_items.count(), 2)
-
-        first_saved_item = saved_items[0]
-        second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "첫번째 아이템")
-        self.assertEqual(first_saved_item.list, mylist)
-        self.assertEqual(second_saved_item.text, "두번째 아이템")
-        self.assertEqual(second_saved_item.list, mylist)
