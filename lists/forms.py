0029d8491e48e27732f90b1458e7e1175679f274
ChaeJeong <pagw1604@gmail.com>
2023-12-01 14:07:28 +0900

    14. list items를 위한 form 추가, Unit Test 추가

--- lists/forms.py
+++ lists/forms.py
@@ -1,11 +1,25 @@
 from django import forms
+from lists.models import Item
 
-class ItemForm(forms.Form):
-    item_text = forms.CharField(label='Item Text', max_length=255, strip=True)
+EMPTY_ITEM_ERROR = "공백은 입력할 수 없습니다."
 
-    def clean_item_text(self):
+
+class ItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('text', )
+        widgets = {
+            'text': forms.TextInput(attrs={
+            'placeholder': 'Enter a to-do item',
+            'class':'form-control form-control-lg',
+            }),
+        }
+        error_messages = {
+            'text': {'required': EMPTY_ITEM_ERROR}
+        }
+
+    def clean_text(self):
         item_text = self.cleaned_data['item_text']
-        # Your custom validation logic if needed
         if not item_text:
             raise forms.ValidationError("공백은 입력할 수 없습니다.")
         return item_text
